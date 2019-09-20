a111edc9956e6e1223e2328c01e5624b5017df47
IBRINEI <55546925+IBRINEI@users.noreply.github.com>
2019-09-20 17:28:41 +0300

    Add files via upload

---
+++ 4HW.py
@@ -0,0 +1,120 @@
+from graph import *
+
+windowSize(1760, 769)
+canvasSize(1760, 769)
+
+
+
+penColor('orange')
+brushColor('orange')
+circle(463, 769, 300)
+
+brushColor(233,200,176)
+penColor(200,200,200)
+circle(463, 384, 200)
+penColor(0,0,0)
+
+brushColor(100,100,255)
+penColor('black')
+circle(528, 349, 35)
+circle(398 , 349, 35)
+brushColor('black')
+circle(528, 349, 10)
+circle(398 , 349, 10)
+
+brushColor('brown')
+polygon([(483,394),(443,394),(463,424)])
+
+brushColor('red')
+polygon([(333, 454), (613, 454), (463, 534)])
+
+
+#def trg(a)
+#brushColor('purple')
+#polygon([(603,524
+brushColor(233,200,176)
+penColor(200,200,200)
+polygon([(230,569),(270,549),(180, 150), (140, 130)])
+polygon([(630,549),(670,569),(880, 150), (840, 130)])
+
+penColor('white')
+circle(160,115, 40)
+circle(870, 115, 40)
+
+
+penColor('black')
+brushColor('orange')
+polygon([(300,629),(220,629),(210,539),(280,489),(320,549)])
+polygon([(700,629),(620, 629), (610, 539), (680, 489), (720, 549)])
+
+brushColor('purple')
+polygon([(293,275), (336,219), (267,205)])
+polygon([(316,230), (378,200), (329,166)])
+polygon([(362,201), (431,186), (377,149)])
+polygon([(417,184), (476,180), (436,144)])
+polygon([(457,187), (509,186), (486,143)])
+polygon([(498,178), (543,200), (525,151)])
+polygon([(532,187), (586,220), (573,152)])
+polygon([(576,206), (621,253), (615,182)])
+polygon([(608,235), (644,289), (674,218)])
+
+
+#copy start
+penColor('orange')
+brushColor('green')
+circle(1263, 769, 300)
+
+brushColor(233,200,176)
+penColor(200,200,200)
+circle(1263, 384, 200)
+penColor(0,0,0)
+
+brushColor(191,200,183)
+penColor('black')
+circle(1328, 349, 35)
+circle(1198 , 349, 35)
+brushColor('black')
+circle(1328, 349, 10)
+circle(1198 , 349, 10)
+
+brushColor('brown')
+polygon([(1283,394),(1243,394),(1263,424)])
+
+brushColor('red')
+polygon([(1133, 454), (1413, 454), (1263, 534)])
+
+
+
+brushColor(233,200,176)
+penColor(200,200,200)
+polygon([(1030,569),(1070,549),(980, 150), (940, 130)])
+polygon([(1430,549),(1470,569),(1680, 150), (1640, 130)])
+
+penColor('white')
+circle(960,115, 40)
+circle(1670, 115, 40)
+
+
+penColor('black')
+brushColor('green')
+polygon([(1100,629),(1020,629),(1010,539),(1080,489),(1120,549)])
+polygon([(1500,629),(1420, 629), (1410, 539), (1480, 489), (1520, 549)])
+
+brushColor('yellow')
+polygon([(1093,275), (1136,219), (1067,205)])
+polygon([(1116,230), (1178,200), (1129,166)])
+polygon([(1162,201), (1231,186), (1177,149)])
+polygon([(1217,184), (1276,180), (1236,144)])
+polygon([(1257,187), (1309,186), (1286,143)])
+polygon([(1298,178), (1343,200), (1325,151)])
+polygon([(1332,187), (1386,220), (1373,152)])
+polygon([(1376,206), (1421,253), (1415,182)])
+polygon([(1408,235), (1444,289), (1474,218)])
+#copy end
+brushColor('black')
+
+
+label('PYTHON is REALLY AMAZING!',00,00, font=('Arial 32', 90, 'bold'), bg='green', foreground='black')
+
+run()
+

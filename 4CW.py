f12b4572f96fe1585822828dae735112c788e80f
IBRINEI <55546925+IBRINEI@users.noreply.github.com>
2019-09-20 17:33:46 +0300

    Add files via upload

---
+++ 4CW.py
@@ -0,0 +1,69 @@
+from graph import *
+
+windowSize(1020, 769)
+canvasSize(1020, 769)
+
+
+
+penColor('orange')
+brushColor('orange')
+circle(463, 769, 300)
+
+brushColor(233,200,176)
+penColor(200,200,200)
+circle(463, 384, 200)
+penColor(0,0,0)
+
+brushColor(100,100,255)
+penColor('black')
+circle(528, 349, 35)
+circle(398 , 349, 35)
+brushColor('black')
+circle(528, 349, 10)
+circle(398 , 349, 10)
+
+brushColor('brown')
+polygon([(483,394),(443,394),(463,424)])
+
+brushColor('red')
+polygon([(333, 454), (613, 454), (463, 534)])
+
+
+#def trg(a)
+#brushColor('purple')
+#polygon([(603,524
+brushColor(233,200,176)
+penColor(200,200,200)
+polygon([(230,569),(270,549),(180, 150), (140, 130)])
+polygon([(630,549),(670,569),(880, 150), (840, 130)])
+
+penColor('white')
+circle(160,115, 40)
+circle(870, 115, 40)
+
+
+penColor('black')
+brushColor('orange')
+polygon([(300,629),(220,629),(210,539),(280,489),(320,549)])
+polygon([(700,629),(620, 629), (610, 539), (680, 489), (720, 549)])
+
+brushColor('purple')
+polygon([(293,275), (336,219), (267,205)])
+polygon([(316,230), (378,200), (329,166)])
+polygon([(362,201), (431,186), (377,149)])
+polygon([(417,184), (476,180), (436,144)])
+polygon([(457,187), (509,186), (486,143)])
+polygon([(498,178), (543,200), (525,151)])
+polygon([(532,187), (586,220), (573,152)])
+polygon([(576,206), (621,253), (615,182)])
+polygon([(608,235), (644,289), (674,218)])
+
+
+
+brushColor('black')
+
+
+label('PYTHON is AMAZING',00,00, font=('Arial 32', 76, 'bold'), bg='green', foreground='black')
+
+run()
+

8f816397195373b9b1b0a293f4d12734c0b69102
IBRINEI <55546925+IBRINEI@users.noreply.github.com>
2019-09-23 09:29:13 +0300

    Update 4HW.py
    More pep8

--- 4HW.py
+++ 4HW.py
@@ -9,12 +9,12 @@ penColor('orange')
 brushColor('orange')
 circle(463, 769, 300)
 
-brushColor(233,200,176)
-penColor(200,200,200)
+brushColor(233, 200, 176)
+penColor(200, 200, 200)
 circle(463, 384, 200)
-penColor(0,0,0)
+penColor(0, 0, 0)
 
-brushColor(100,100,255)
+brushColor(100, 100, 255)
 penColor('black')
 circle(528, 349, 35)
 circle(398 , 349, 35)
@@ -23,29 +23,27 @@ circle(528, 349, 10)
 circle(398 , 349, 10)
 
 brushColor('brown')
-polygon([(483,394),(443,394),(463,424)])
+polygon([(483, 394), (443, 394), (463, 424)])
 
 brushColor('red')
 polygon([(333, 454), (613, 454), (463, 534)])
 
 
-#def trg(a)
-#brushColor('purple')
-#polygon([(603,524
-brushColor(233,200,176)
-penColor(200,200,200)
-polygon([(230,569),(270,549),(180, 150), (140, 130)])
-polygon([(630,549),(670,569),(880, 150), (840, 130)])
+
+brushColor(233, 200, 176)
+penColor(200, 200, 200)
+polygon([(230, 569), (270, 549), (180, 150), (140, 130)])
+polygon([(630, 549), (670, 569), (880, 150), (840, 130)])
 
 penColor('white')
-circle(160,115, 40)
+circle(160, 115, 40)
 circle(870, 115, 40)
 
 
 penColor('black')
 brushColor('orange')
-polygon([(300,629),(220,629),(210,539),(280,489),(320,549)])
-polygon([(700,629),(620, 629), (610, 539), (680, 489), (720, 549)])
+polygon([(300, 629), (220, 629), (210, 539), (280, 489), (320, 549)])
+polygon([(700, 629), (620, 629), (610, 539), (680, 489), (720, 549)])
 
 brushColor('purple')
 polygon([(293,275), (336,219), (267,205)])
@@ -64,12 +62,12 @@ penColor('orange')
 brushColor('green')
 circle(1263, 769, 300)
 
-brushColor(233,200,176)
-penColor(200,200,200)
+brushColor(233, 200, 176)
+penColor(200, 200, 200)
 circle(1263, 384, 200)
-penColor(0,0,0)
+penColor(0, 0, 0)
 
-brushColor(191,200,183)
+brushColor(191, 200, 183)
 penColor('black')
 circle(1328, 349, 35)
 circle(1198 , 349, 35)
@@ -78,27 +76,27 @@ circle(1328, 349, 10)
 circle(1198 , 349, 10)
 
 brushColor('brown')
-polygon([(1283,394),(1243,394),(1263,424)])
+polygon([(1283, 394), (1243, 394), (1263, 424)])
 
 brushColor('red')
 polygon([(1133, 454), (1413, 454), (1263, 534)])
 
 
 
-brushColor(233,200,176)
-penColor(200,200,200)
-polygon([(1030,569),(1070,549),(980, 150), (940, 130)])
-polygon([(1430,549),(1470,569),(1680, 150), (1640, 130)])
+brushColor(233, 200, 176)
+penColor(200, 200, 200)
+polygon([(1030, 569), (1070, 549), (980, 150), (940, 130)])
+polygon([(1430, 549), (1470, 569), (1680, 150), (1640, 130)])
 
 penColor('white')
-circle(960,115, 40)
+circle(960, 115, 40)
 circle(1670, 115, 40)
 
 
 penColor('black')
 brushColor('green')
-polygon([(1100,629),(1020,629),(1010,539),(1080,489),(1120,549)])
-polygon([(1500,629),(1420, 629), (1410, 539), (1480, 489), (1520, 549)])
+polygon([(1100, 629), (1020, 629), (1010, 539), (1080, 489), (1120, 549)])
+polygon([(1500, 629), (1420, 629), (1410, 539), (1480, 489), (1520, 549)])
 
 brushColor('yellow')
 polygon([(1093,275), (1136,219), (1067,205)])
@@ -114,7 +112,7 @@ polygon([(1408,235), (1444,289), (1474,218)])
 brushColor('black')
 
 
-label('PYTHON is REALLY AMAZING!',00,00, font=('Arial 32', 90, 'bold'), bg='green', foreground='black')
+label('PYTHON is REALLY AMAZING!', 00, 00, font=('Arial 32', 90, 'bold'), bg='green', foreground='black')
 
 run()
 
